51e60281b2121698c241cb1c0e6f597ddf935fc6
nsoft-andrey <a.lukyanchyk@nord-soft.com>
2018-01-18 14:20:37 +0300

    started work on the token protection

--- lambda/index.js
+++ lambda/index.js
@@ -8,13 +8,27 @@ const Sharp = require('sharp');
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
+const SECRET_KEY = process.env.SECRET_KEY;
+const RESIZED_IMAGES_PATH = process.env.RESIZED_IMAGES_PATH;
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
+  var key = event.queryStringParameters.key;
+  var key_with_token = key.split('?t=');
+
+  if(key_with_token.length != 2) {
+      return context.fail("Permissions denied");
+  }
+
+  key = key_with_token[0];
+  const token = key_with_token[1];
   const match = key.match(/(\d+)x(\d+)\/(.*)/);
   const width = parseInt(match[1], 10);
   const height = parseInt(match[2], 10);
   const originalKey = match[3];
+
+  if(token != 'supersecretkey') {
+      return context.fail("Permissions denied");
+  }
 
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
